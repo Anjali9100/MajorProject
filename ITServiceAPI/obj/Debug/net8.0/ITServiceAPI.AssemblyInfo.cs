b0a5b0efc56e5e2c2aa78a395ad6ceb2be128c88
Anjali Kumari <64739765+Anjali9100@users.noreply.github.com>
2024-06-18 20:53:45 +0530

    redirection

--- ITServiceAPI/obj/Debug/net8.0/ITServiceAPI.AssemblyInfo.cs
+++ ITServiceAPI/obj/Debug/net8.0/ITServiceAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ITServiceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e5fc9be70eebf857a1766ff71b628808e0380f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+113e0c59923ea16cfd86cae2d0717f8d39cb1fc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ITServiceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ITServiceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
